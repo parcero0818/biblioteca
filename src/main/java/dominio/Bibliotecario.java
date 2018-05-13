348f3d0aeaa8897ad54085f7661511186e6f5846
parcero0818 <john.sgv@outlook.com>
2018-05-12 22:28:11 -0500

    commit

---
+++ src/main/java/dominio/Bibliotecario.java
@@ -0,0 +1,29 @@
+package dominio;
+
+import dominio.repositorio.RepositorioLibro;
+import dominio.repositorio.RepositorioPrestamo;
+
+public class Bibliotecario {
+
+	public static final String EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE = "El libro no se encuentra disponible";
+
+	private RepositorioLibro repositorioLibro;
+	private RepositorioPrestamo repositorioPrestamo;
+
+	public Bibliotecario(RepositorioLibro repositorioLibro, RepositorioPrestamo repositorioPrestamo) {
+		this.repositorioLibro = repositorioLibro;
+		this.repositorioPrestamo = repositorioPrestamo;
+
+	}
+
+	public void prestar(String isbn) {
+
+		throw new UnsupportedOperationException("Método pendiente por implementar");
+
+	}
+
+	public boolean esPrestado(String isbn) {
+		return false;
+	}
+
+}
