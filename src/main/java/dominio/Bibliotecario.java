9766d1a71b567f6b8e489215f6fe5c26c7baae15
parcero0818 <john.sgv@outlook.com>
2018-05-12 23:57:17 -0500

    cambios
    Signed-off-by: parcero0818 <john.sgv@outlook.com>

--- src/main/java/dominio/Bibliotecario.java
+++ src/main/java/dominio/Bibliotecario.java
@@ -1,5 +1,6 @@
 package dominio;
 
+import dominio.excepcion.PrestamoException;
 import dominio.repositorio.RepositorioLibro;
 import dominio.repositorio.RepositorioPrestamo;
 
@@ -16,14 +17,42 @@ public class Bibliotecario {
 
 	}
 
-	public void prestar(String isbn) {
+	public String prestar(String isbn) {
+		if(esPrestado(isbn)) {
+			throw new PrestamoException("El libro no se encuentra disponible");
+		}else if(isbnPalindromo(isbn)){
+			return "Los libros palindromos solo se pueden utilizar en la biblioteca";
+		}else{
+			Libro libro = new Libro(isbn, "", 0);
+			Prestamo prestamo = new Prestamo(null, libro, null, "usuario1");
+			repositorioPrestamo.agregar(prestamo);
+		}
+		esPrestado(isbn);
 
-		throw new UnsupportedOperationException("Método pendiente por implementar");
-
+		return "true";
 	}
 
 	public boolean esPrestado(String isbn) {
+		Libro libro = repositorioPrestamo.obtenerLibroPrestadoPorIsbn(isbn);
+		if(null != libro) {
+			return true;
+		}
 		return false;
+	}
+	
+	/**
+	 * Metodo que permite identificar si un isbn de un libro es palindrono
+	 * @param isbn
+	 * @return
+	 */
+	private boolean isbnPalindromo(String isbn) {
+		if (isbn == null) {
+			return false;
+		} else if (isbn.isEmpty() || isbn.length() == 1) {
+			return true;
+		}
+		int len = isbn.length() - 1;
+		return isbn.charAt(0) == isbn.charAt(len) && isbnPalindromo(isbn.substring(1, len));
 	}
 
 }
