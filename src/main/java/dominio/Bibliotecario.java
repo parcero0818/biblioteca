79f617868ecf5b04482a60cb6b8523bc1a37345b
parcero0818 <john.sgv@outlook.com>
2018-05-13 16:34:14 -0500

    suma de los digitos del isbn
    Signed-off-by: parcero0818 <john.sgv@outlook.com>

--- src/main/java/dominio/Bibliotecario.java
+++ src/main/java/dominio/Bibliotecario.java
@@ -21,27 +21,42 @@ public class Bibliotecario {
 
 	}
 
+	/***
+	 * Metodo que permite realizar el prestamo de un libro
+	 * @param libro
+	 * @param nombreUsuario
+	 */
 	public void prestar(Libro libro, String nombreUsuario) {
-		if(esPrestado(libro.getIsbn())) {
+		if (esPrestado(libro.getIsbn())) {
 			throw new PrestamoException("El libro no se encuentra disponible");
-		}else if(isbnPalindromo(libro.getIsbn())){
+		} else if (isbnPalindromo(libro.getIsbn())) {
 			throw new PrestamoPalindromoException("Los libros palíndromos solo se pueden utilizar en la biblioteca");
-		}else{
-			Prestamo prestamo = new Prestamo(new Date(), libro, null, nombreUsuario);
-			repositorioPrestamo.agregar(prestamo);
+		} else {
+			if(sumaDigitosIsbn(libro.getIsbn(), 0) > 30) {
+				System.out.println("suman mas de 30");
+			}else {
+				Prestamo prestamo = new Prestamo(new Date(), libro, null, nombreUsuario);
+				repositorioPrestamo.agregar(prestamo);
+			}
 		}
 	}
-
+	/***
+	 * Metodo que permite saber si un libro ya se encuentra prestado
+	 * @param isbn
+	 * @return
+	 */
 	public boolean esPrestado(String isbn) {
 		Libro libro = repositorioPrestamo.obtenerLibroPrestadoPorIsbn(isbn);
-		if(null != libro) {
+		if (null != libro) {
 			return true;
 		}
 		return false;
 	}
-	
-	/**
-	 * Metodo que permite identificar si un isbn de un libro es palindrono
+
+	/***
+	 * Metodo recursivo que permite identificar si un isbn de un libro es palindrono
+	 * 
+	 * @author Edward
 	 * @param isbn
 	 * @return
 	 */
@@ -53,6 +68,30 @@ public class Bibliotecario {
 		}
 		int len = isbn.length() - 1;
 		return isbn.charAt(0) == isbn.charAt(len) && isbnPalindromo(isbn.substring(1, len));
+	}
+
+	/***
+	 * Metodo recursivo que permite sumar los digitos del isbn para determinar si al
+	 * libro que se presta se de debe calcular la fecha maxima de entrega
+	 * @author Edward
+	 * @param isbn
+	 * @param suma
+	 * @return
+	 */
+	public int sumaDigitosIsbn(String isbn, int suma) {
+		if (isbn == null || isbn.isEmpty()) {
+			return suma;
+		} else if (isbn.length() == 1) {
+			if (Character.isDigit(isbn.charAt(0))) {
+				suma += Integer.parseInt(String.valueOf(isbn.charAt(0)));
+			}
+			return suma;
+		}
+		int len = isbn.length() - 1;
+		if (Character.isDigit(isbn.charAt(len))) {
+			suma += Integer.parseInt(String.valueOf(isbn.charAt(len)));
+		}
+		return sumaDigitosIsbn(isbn.substring(0, len), suma);
 	}
 
 }
